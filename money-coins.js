1408c0a205d4cfc9a2d8c734d1558a609957fdac
Hacktiv8 <hacktiv8@H8JKT003.local>
2018-02-05 16:12:08 +0700

    money coins solved

--- money-coins.js
+++ money-coins.js
@@ -1,5 +1,13 @@
-function moneyCoins (num) {
-  // your implementation code here
+function moneyCoins(num) {
+  var pecahan = [1, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000]
+  var coinConverted = [];
+  for(var i=pecahan.length-1; i>=0; i--){
+    while(num >= pecahan[i]){
+      num -= pecahan[i];
+      coinConverted.push(pecahan[i]);
+    }
+  }
+  return coinConverted;
 }
 
 // Drive code
