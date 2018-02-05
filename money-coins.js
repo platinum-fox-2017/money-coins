233b2d9783d9df41a31680e68799b084667358dc
Kevin Himawan Hie <kevinhimawanhie@Kevins-MacBook-Pro.local>
2018-02-05 22:15:16 +0700

    Updated

--- money-coins.js
+++ money-coins.js
@@ -1,6 +1,34 @@
+let hasil = []
 function moneyCoins (num) {
-  // your implementation code here
+  let kamus = [10000,5000,2000,1000,500,200,100,50,20,10,1]
+    if(num <= 0){
+        return hasil
+    }else{
+
+        // for(let [index,value] of kamus.entries()){
+        //     if(num >= value){
+        //         hasil.push(value)
+        //         return moneyCoins(num -value)
+                
+        //     }
+        // }
+
+        const output = kamus.filter(each =>{
+            let output = num / each
+            
+            if(output >=1){
+                return output
+            }
+        });
+        
+        hasil.push(output[0])
+
+        return moneyCoins(num-output[0])
+
+    }
 }
+
+
 
 // Drive code
 console.log(moneyCoins(543))
