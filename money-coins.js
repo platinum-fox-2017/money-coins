10c616768b9e1428b0ef031183c7e5cb931533ad
srohimah <sitirohimach@gmail.com>
2018-02-05 15:13:03 +0700

    money coins

--- money-coins.js
+++ money-coins.js
@@ -1,5 +1,13 @@
 function moneyCoins (num) {
-  // your implementation code here
+  let pecahan=[];
+    let arrNum = [10000, 5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 1];
+    for(let i=0; i<arrNum.length; i++){
+        while(num>=arrNum[i]){
+            pecahan.push(arrNum[i])
+            num-=arrNum[i]
+        }
+    }
+    return pecahan  
 }
 
 // Drive code
