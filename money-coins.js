07f56381cfe30592c96ee3aabf0a0babc1e42175
Fadhilmch <fadhil.mochammad1095@gmail.com>
2018-02-05 20:52:13 +0700

    added solution 2

--- money-coins.js
+++ money-coins.js
@@ -1,4 +1,4 @@
-// Coin dengan pecahan: 1,10,20,50,100,200,500,1000,2000,5000,10000
+// Solution 1  - note recursive
 function moneyCoins (num) {
   // your implementation code here
   var arrayCoin = [1,10,20,50,100,200,500,1000,2000,5000,10000];
@@ -7,7 +7,7 @@ function moneyCoins (num) {
   while(num>=0){
     if(num==0)
       return result;
-    //console.log(num);
+
     var tempNumber = Math.floor(num/arrayCoin[pointer]);
     if(tempNumber>0){
       for(var i=0;i<tempNumber;i++)
@@ -20,6 +20,23 @@ function moneyCoins (num) {
     }
   }
   return result;
+}
+
+// Solution 2 - recursive
+function moneyCoins (num) {
+  // your implementation code here
+  var arrayCoin = [10000,5000,2000,1000,500,200,100,50,20,10,1];
+  var result = [];
+
+  if(num == 0)
+    return [];
+
+  for(var i = 0; i<arrayCoin.length;i++){
+    if(num/arrayCoin[i]>=1){
+      result.push(arrayCoin[i]);
+      return result.concat(moneyCoins(num-arrayCoin[i]));
+    }
+  }
 }
 
 // Drive code
