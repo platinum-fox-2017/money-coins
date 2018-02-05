c8b5aa62251f022797aa59a0a3ab7a846bbcd145
Eki Fakhrureza <ekifakhrureza@gmail.com>
2018-02-05 15:39:46 +0700

    MONEY COIN JS

--- money-coins.js
+++ money-coins.js
@@ -1,13 +1,24 @@
-function moneyCoins (num) {
-  // your implementation code here
+function moneyCoins(num)
+{
+var counter=[1,10,20,50,100,200,500,1000,2000,5000,10000];
+var tempArr=[];
+
+  for(var i=counter.length-1;i>=0;i--)
+    {
+      while(num-counter[i]>=0)
+      {
+         num=num-counter[i];
+        tempArr.push(counter[i])
+      }
+     
+    }
+  return tempArr;
 }
 
-// Drive code
-console.log(moneyCoins(543))
-//output : [ 500, 20, 20, 1, 1, 1 ]
+console.log(moneyCoins(543));
+//output [500,20,20,1,1,1]
+console.log(moneyCoins(7752));
+//output [5000,2000,500,200,50,1,1]
+console.log(moneyCoins(37454));
+//[10000,10000,10000,5000,2000,200,200,50,1,1,1,1]
 
-console.log(moneyCoins(7752))
-//output : [ 5000, 2000, 500, 200, 50, 1, 1 ]
-
-console.log(moneyCoins(37454))
-// [ 10000, 10000, 10000, 5000, 2000, 200, 200, 50, 1, 1, 1, 1 ]
